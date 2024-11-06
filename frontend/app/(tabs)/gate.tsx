bc55dba95ce7e66f8481d6d34f312235e4071584
pohsaykeong <pohsaykeong@gmail.com>
2024-11-06 12:36:05 +0800

    feat: add qr scan loading

--- frontend/app/(tabs)/gate.tsx
+++ frontend/app/(tabs)/gate.tsx
@@ -1,9 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Html5Qrcode } from 'html5-qrcode';
+import { ActivityIndicator, View, StyleSheet, Text } from 'react-native';
+import Icon from "react-native-vector-icons/FontAwesome";
 
 export default function WebQRCodeScanner() {
+    const [isLoading, setIsLoading] = useState(false);
+    const [isVerified, setIsVerified] = useState(null);
+
     const handleQRCodeScanned = (data: string) => {
         console.log('Scanned QR Code:', data);
+        setIsLoading(true);
     };
 
     useEffect(() => {
@@ -22,5 +28,60 @@ export default function WebQRCodeScanner() {
         };
     }, []);
 
-    return <div id="reader" style={{ width: '100%', height: '100%' }} />;
+    return <View style={styles.container}>
+
+        {isVerified !== null && (
+            <View style={styles.overlay}>
+
+                {isVerified ? (
+                    <>
+                        <Icon name="check-circle" size={80} color="green" />
+                        <Text style={styles.verifiedText}>Verified</Text>
+                    </>
+                ) : (
+                    <>
+                        <Icon name="times-circle" size={80} color="red" />
+                        <Text style={styles.unverifiedText}>Not Verified</Text>
+                    </>
+                )}
+            </View>
+        )}
+
+        {isLoading && (
+            <View style={styles.overlay}>
+                <ActivityIndicator size="large" color="#ffffff" />
+            </View>
+        )}
+
+        <div id="reader" style={{ width: '100%', height: '100%' }} />
+    </View>
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    overlay: {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        justifyContent: "center",
+        alignItems: "center",
+        zIndex: 2,
+        backgroundColor: "rgba(0, 0, 0, 0.6)",
+    },
+    verifiedText: {
+        color: "green",
+        fontSize: 24,
+        fontWeight: "bold",
+    },
+    unverifiedText: {
+        color: "red",
+        fontSize: 24,
+        fontWeight: "bold",
+    },
+});
